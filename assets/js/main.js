03c9f68d6d8ae230bf711e0baa2e4ac98d76cb59
juliano <juliano.pedraca@gmail.com>
2022-11-29 09:55:41 -0300

    added modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,80 +1,86 @@
-const pokemonList = document.getElementById('pokemonList')
-const loadMoreButton = document.getElementById('loadMoreButton')
+const pokemonList = document.getElementById("pokemonList");
+const loadMoreButton = document.getElementById("loadMoreButton");
 
-const modal = document.querySelector(".modal");
-const overlay = document.querySelector(".overlay");
-const openModalBtn = document.querySelector(".btn-open");
+const overlay = document.getElementById("overlay");
 const closeModalBtn = document.querySelector(".btn-close");
 
-
-const maxRecords = 151
-const limit = 10
+const maxRecords = 151;
+const limit = 10;
 let offset = 0;
 
 function convertPokemonToLi(pokemon) {
-    return `
-        <li class="pokemon ${pokemon.type}">
+  return `
+        <li class="pokemon ${pokemon.type}" onclick="openModal('${
+    pokemon.name
+  }')">
             <span class="number">#${pokemon.number}</span>
             <span class="name">${pokemon.name}</span>
             <div class="detail">
                 <ol class="types">
-                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                    ${pokemon.types
+                      .map((type) => `<li class="type ${type}">${type}</li>`)
+                      .join("")}
                 </ol>
                 <img src="${pokemon.photo}"
                      alt="${pokemon.name}">
             </div>
         </li>
-    `
+        <div class="modal hidden" id="${pokemon.name}Modal">
+        <img src="${pokemon.photo}"
+                     alt="${pokemon.name}">
+        </div>
+                
+    `;
 }
-
-
 
 function loadPokemonItens(offset, limit) {
-    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
-        const newHtml = pokemons.map(convertPokemonToLi).join('')
-        pokemonList.innerHTML += newHtml
-    })
+  pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+    const newHtml = pokemons.map(convertPokemonToLi).join("");
+    pokemonList.innerHTML += newHtml;
+  });
 }
 
-loadPokemonItens(offset, limit)
+function openModal(pokemon) {
+  const pokeModal = document.getElementById(pokemon + "Modal");
+  pokeModal.classList.remove("hidden");
+  const overlay = document.getElementById("overlay");
+  overlay.classList.remove("hidden");
+}
 
-loadMoreButton.addEventListener('click', () => {
-    offset += limit
-    const qtdRecordsWithNexPage = offset + limit
+loadPokemonItens(offset, limit);
 
-    if (qtdRecordsWithNexPage >= maxRecords) {
-        const newLimit = maxRecords - offset
-        loadPokemonItens(offset, newLimit)
+loadMoreButton.addEventListener("click", () => {
+  offset += limit;
+  const qtdRecordsWithNexPage = offset + limit;
 
-        loadMoreButton.parentElement.removeChild(loadMoreButton)
-    } else {
-        loadPokemonItens(offset, limit)
-    }
-})
+  if (qtdRecordsWithNexPage >= maxRecords) {
+    const newLimit = maxRecords - offset;
+    loadPokemonItens(offset, newLimit);
 
-// open modal function
-const openModal = function () {
-    modal.classList.remove("hidden");
-    overlay.classList.remove("hidden");
-  };
-
-  // open modal event
-  openModalBtn.addEventListener("click", openModal);
+    loadMoreButton.parentElement.removeChild(loadMoreButton);
+  } else {
+    loadPokemonItens(offset, limit);
+  }
+});
 
 // close modal function
-const closeModal = function () {
-    modal.classList.add("hidden");
-    overlay.classList.add("hidden");
-  };
-  
-  // close the modal when the close button and overlay is clicked
-  closeModalBtn.addEventListener("click", closeModal);
-  overlay.addEventListener("click", closeModal);
-  
-  // close modal when the Esc key is pressed
-  document.addEventListener("keydown", function (e) {
-    if (e.key === "Escape" && !modal.classList.contains("hidden")) {
-      closeModal();
+function closeModal() {
+  const modal = document.querySelectorAll(".modal");
+  for (const popModal of modal) {
+    if (popModal.classList.contains("hidden") === false) {
+      popModal.classList.add("hidden");
     }
-  });
-  
+  }
+  const overlay = document.getElementById("overlay");
+  overlay.classList.add("hidden");
+}
+
+// close the modal when the close button and overlay is clicked
+overlay.addEventListener("click", closeModal);
+
+// close modal when the Esc key is pressed
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape") {
+    closeModal();
+  }
+});
