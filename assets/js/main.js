3d3f52465507115b6c6cb708825254860c88eaef
juliano <juliano.pedraca@gmail.com>
2022-12-07 17:29:39 -0300

    style: added background to all model types

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,8 +7,6 @@ const maxRecords = 151;
 const limit = 10;
 let offset = 0;
 
-
-
 function convertPokemonToLi(pokemon) {
   return `
 <li class="pokemon ${pokemon.type}" onclick="openModal('${pokemon.name}')">
